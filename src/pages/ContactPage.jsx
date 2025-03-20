2828e81f0e781badbaa86d200ec110ebbf6b3599
Eliza Pant <elizapant.github@gmail.com>
2025-03-20 09:49:48 +0545

    erf34

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import Navbar from '../components/Navbar'
+
+
+import DirectContactUs from "../components/DirectContactUs";
+import Footer from "../components/Footer";
+
+import Reviews from "../components/Reviews";
+import Contact from "../components/HotelMap";
 
 const ContactPage = () => {
+   useEffect(() => {
+      // Scroll to the top when the page loads
+      window.scrollTo(0, 0);
+    }, []);
   return (
-    <div>ContactPage</div>
+    <div>
+      <Navbar/>
+      <Contact className="mt-4"/>
+  <Reviews/>
+    <DirectContactUs/>
+      <Footer/>
+      
+    </div>
   )
 }
 
--- src/pages/ServicesPage.jsx
+++ src/pages/ServicesPage.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import Navbar from '../components/Navbar'
+import About from '../components/About'
+
+import DirectContactUs from "../components/DirectContactUs";
+import Footer from "../components/Footer";
+import ServicesSection from "../components/Services";
+import FoodSection from "../components/FoodSection";
+import HotelFacilitiesComponent from "../components/HotelFacilitiesComponent";
+import SpaGallery from "../components/SpaGallery";
 
 const ServicesPage = () => {
+   useEffect(() => {
+      // Scroll to the top when the page loads
+      window.scrollTo(0, 0);
+    }, []);
   return (
-    <div>ServicesPage</div>
+    <div>
+      <Navbar/>
+    <ServicesSection/>
+    <FoodSection/>
+    <HotelFacilitiesComponent/>
+    <SpaGallery/>
+    <DirectContactUs/>
+      <Footer/>
+      
+    </div>
   )
 }
 
